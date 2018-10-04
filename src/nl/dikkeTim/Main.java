f458e3eb1d73f58212186c85990cb3a0ac55908b
Steve Galili <steve@meetfeli.com>
2018-10-04 12:19:28 +0200

    printing files from various locations

--- src/nl/dikkeTim/Main.java
+++ src/nl/dikkeTim/Main.java
@@ -1,8 +1,32 @@
 package nl.dikkeTim;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Path path = FileSystems.getDefault().getPath("workingdir.txt");
+        printFile(path);
+        path = FileSystems.getDefault().getPath("files", "subdir.txt");
+        printFile(path);
+        path = Paths.get("/home/vangalilea/Aiden/JavaCourse/outsidedir.txt");
+        printFile(path);
+    }
+
+    private static void printFile(Path path) {
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
